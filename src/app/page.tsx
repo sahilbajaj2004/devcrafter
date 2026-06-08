5b1ed848f8e9b93c5eea1f94246fa844336c226a
Sahil Bajaj <sahilbajaj0941@gmail.com>
2026-06-08 22:31:35 +0530

    see

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,10 @@ import ProjectsSection from "@/components/ProjectsSection";
 import ContactSection from "@/components/ContactSection";
 import TestimonialsSection from "@/components/TestimonialsSection";
 import FaqSection from "@/components/FaqSection";
-import Ribbons from "@/components/ui/RibbonsCursor";
 
 export default function SoberDevWebsite() {
   return (
     <>
-    <Ribbons/>
     <div className="min-h-screen">
       <Navbar />
       <HeroSection />
